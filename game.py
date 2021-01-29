05e035d2be61c4c2f46a4bdbfbbc965bdd71eeb4
Monica <monica@hackbrightacademy.com>
2021-01-29 06:52:56 +0000

    Set parameters, set if statements, and provided number of attempts upon completion

--- game.py
+++ game.py
@@ -1,4 +1,32 @@
 """A number-guessing game."""
+import random 
+
+answer = random.randint (1, 100)
 
 print("hello, what's your name? >")
+
+name = input ("> ")
+
+print (name, "I'm thinking of a number between 1 and 100.")
+print ()
+print ("try to guess my number > ")
+
+tries = 0
+
+while True:
+    guess = input (" > ")
+    guess = int (guess)
+    tries = tries+1
+
+    if guess < answer: 
+        print ("too low! Try again > ")
+    
+    
+    elif guess > answer: 
+        print ("too high! try again >")
+    
+    else:
+        print ("Success!")
+        print ("It only took you", tries, "tries!")
+        break
 
